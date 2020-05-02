1130fac2a4a57ff8ab95c64458e94e4cd581454f
jk <jk@vin.ovh>
2020-05-02 08:26:20 -0500

    updated scripts

--- update-repo.sh
+++ update-repo.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-VER=80.0.3987.106-1
+VER=81.0.4044.129-2
 PKG=ungoogled-chromium-$VER-x86_64.pkg.tar.xz
 SIG=$PKG.sig
 
+echo "Signing package..."
+rm -f $SIG
 gpg -o $SIG --detach-sign $PKG
-repo-add -s -R jk-aur.db.tar.gz $PKG
+echo "Adding to repository files..."
+repo-add -q -s -R jk-aur.db.tar.gz $PKG
+echo "Sending repository files to server..."
+rsync -q -e ssh -Phazz jk-aur* $SIG lyra:/home/jk/Repo/
 
-rsync -e ssh -Phazz jk-aur* $SIG lyra:/home/jk/Repo/
+echo "Downloading package on server..."
+ssh lyra "zsh -s $PKG" <<"ENDSSH"
+    PKG=$1
+    URL="https://download.opensuse.org/repositories/home:/justkidding:/arch/standard/x86_64/${PKG}"
+    cd /home/jk/Repo/
+    wget -q $URL
+    cd /srv/http/repo/arch/x86_64/
+    rm -f *
+    mv /home/jk/Repo/* .
+ENDSSH
+echo "Done."
