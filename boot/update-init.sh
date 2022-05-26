392d0d1b1515fb2ad7b94425594b54043a7adc59
JustKidding <jk@vin.ovh>
2022-05-25 20:27:49 -0500

    u

--- boot/update-init.sh
+++ boot/update-init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-KERNEL=linux
-CPU=amd
+KERNEL=linux-zen
+CPU=intel
 
 INIT=/boot/initramfs-${KERNEL}-${CPU}.img
 ESP=/efi/EFI/Linux
 
-mkinitcpio -p ${KERNEL}
+#mkinitcpio -p ${KERNEL}
 cat /boot/${CPU}-ucode.img /boot/initramfs-${KERNEL}.img > ${INIT}
 
 objcopy \
@@ -16,6 +16,6 @@ objcopy \
 	--add-section .initrd="${INIT}" --change-section-vma .initrd=0x3000000 \
 	"/usr/lib/systemd/boot/efi/linuxx64.efi.stub" "${ESP}/linux.efi"
 
-pesign -i "${ESP}/linux.efi" -o "${ESP}/arch.efi" -c MOK -s -f
+#pesign -i "${ESP}/linux.efi" -o "${ESP}/arch.efi" -c MOK -s -f
 
-rm "${ESP}/linux.efi" ${INIT}
+#rm "${ESP}/linux.efi" ${INIT}
